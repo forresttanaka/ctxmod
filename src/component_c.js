3c9c9aa31dd4d8f4fcbb7307e0756be325e1bfe5
Forrest Tanaka <forrest@forrest-tanaka.com>
2020-07-24 13:43:47 -0700

    Add regular context

--- src/component_c.js
+++ src/component_c.js
@@ -3,14 +3,29 @@ import ComponentE from './component_e';
 import { GlobalCtxmod } from './ctxmod';
 
 
+/**
+ * Create a ctxmod just for Component C and its children to share.
+ */
+export const CBranchContext = React.createContext(null);
+
+
 const ComponentC = () => {
+    const [annunciated, setAnnunciated] = React.useState(false);
     const { moduleName } = React.useContext(GlobalCtxmod);
+
+    const handleCheckbox = (e) => {
+        setAnnunciated(e.target.checked);
+    };
 
     return (
         <div className="component component--c">
-            <h2>Component C</h2>
-            <div>Name: {moduleName.name || <i>none</i>}</div>
-            <ComponentE />
+            <CBranchContext.Provider value={{ annunciated, setAnnunciated }}>
+                <h2>Component C</h2>
+                <div>Name: {moduleName.name || <i>none</i>}</div>
+                <input type="checkbox" id="annunciated" name="annunciated" checked={annunciated} onChange={handleCheckbox} />
+                <label htmlFor="annunciated">Annunciated</label>
+                <ComponentE />
+            </CBranchContext.Provider>
         </div>
     );
 };
--- src/component_f.js
+++ src/component_f.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import { GlobalCtxmod } from './ctxmod';
 import CounterControls from './counter';
+import { CBranchContext } from './component_c';
 
 
 const ComponentF = () => {
     const { ctxmodCounter } = React.useContext(GlobalCtxmod);
+    const { annunciated } = React.useContext(CBranchContext);
 
     return (
         <div className="component component--f">
             <h2>Component F</h2>
             <div>Counter: {ctxmodCounter.counter}</div>
             <CounterControls />
+            <div className={`annunciator${annunciated ? ' annunciator--on' : ''}`} />
         </div>
     );
 };
